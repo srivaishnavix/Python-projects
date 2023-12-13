9b03a0d90fcd82a87201f6092a10e9a8959c8581
srivasihnavix <vinnybhamidipati@gmail.com>
2023-12-13 23:54:55 +0530

    started a new file tic tac toe ai version

---
+++ TIC TAC TOE AI/player.py
@@ -0,0 +1,37 @@
+import math
+import random
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter#here letter is the input we take either x or o
+
+    def get_move(self, game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        #get a random valid sport for our next move
+        square = random.choice(game.available_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (1-9):')
+            try:
+                val = int(square)-1
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True 
+            except ValueError:
+                print("Invalid square. Try again.")
+
+        return val
