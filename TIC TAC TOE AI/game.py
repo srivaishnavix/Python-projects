bfed278170ed455f9b4c859edbfee8534127b60c
srivasihnavix <vinnybhamidipati@gmail.com>
2023-12-16 14:12:03 +0530

    debugging done, code running

--- TIC TAC TOE AI/game.py
+++ TIC TAC TOE AI/game.py
@@ -1,6 +1,6 @@
 import time
 import math
-from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, SmartComputerPlayer
 
 class TicTacToe:
     def __init__(self):
@@ -18,7 +18,7 @@ class TicTacToe:
     @staticmethod
     def print_board_nums():
         #0 | 1 | 2 etc (to show us which number corresponds to a box)
-        number_board = [[str(i+1) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
         for row in number_board:
             print('| '+' | '.join(row)+' |')
 
@@ -97,6 +97,6 @@ def play(game, x_player, o_player, print_game=True):
 
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
-    o_player = GeniusComputerPlayer('O')
+    o_player = SmartComputerPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
--- TIC TAC TOE AI/player.py
+++ TIC TAC TOE AI/player.py
@@ -1,22 +1,15 @@
 import math
 import random
 
-class Player:
+
+class Player():
     def __init__(self, letter):
-        self.letter = letter#here letter is the input we take either x or o
+        self.letter = letter
 
     def get_move(self, game):
         pass
 
-class RandomComputerPlayer(Player):
-    def __init__(self, letter):
-        super().__init__(letter)
-
-    def get_move(self, game):
-        #get a random valid sport for our next move
-        square = random.choice(game.available_moves())
-        return square
-
+    
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
@@ -25,23 +18,31 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-8):')
+            square = input(self.letter +'\'s turn. Input move (0-8):')
             try:
                 val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
-                valid_square = True 
+                valid_square = True
             except ValueError:
-                print("Invalid square. Try again.")
-
+                print('Invalid square. Try again.')
         return val
-    
-class GeniusComputerPlayer(Player):
+
+
+class RandomComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        if len(game.available_moves()) ==9:
+        square = random.choice(game.available_moves())
+        return square
+
+class SmartComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
             square = random.choice(game.available_moves())
         else:
             square = self.minimax(game, self.letter)['position']
@@ -50,25 +51,18 @@ class GeniusComputerPlayer(Player):
     def minimax(self, state, player):
         max_player = self.letter
         other_player = 'O' if player == 'X' else 'X'
-    
 
         if state.current_winner == other_player:
-            return {
-                'position': None,
-                'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)
-            }
-        
+            return{'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
         elif not state.empty_squares():
-            return { 'position': None, 'score': 0}
+            return{'position': None, 'score': 0}
         
         if player == max_player:
-            best = {'position':None, 'score': -math.inf}
+            best = {'position': None, 'score': -math.inf}
         else:
-            best = {'position':None, 'score': math.inf}
-
+            best = {'position': None, 'score': math.inf}
         for possible_move in state.available_moves():
             state.make_move(possible_move, player)
-
             sim_score = self.minimax(state, other_player)
 
             state.board[possible_move] = ' '
@@ -78,8 +72,7 @@ class GeniusComputerPlayer(Player):
             if player == max_player:
                 if sim_score['score'] > best['score']:
                     best = sim_score
-                else:
-                    if sim_score['score'] < best['score']:
-                        best = sim_score
-
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
         return best
