0a915f40d7cb0edb4a477c89387512c2ccd14c08
srivasihnavix <vinnybhamidipati@gmail.com>
2023-12-22 00:00:17 +0530

    added computer guess version

--- guess random number.py
+++ guess random number.py
@@ -12,7 +12,25 @@ def guess(x):
             print('Sorry, guess again. Too low. ')
         elif guess > random_number:
             print('Sorry, guess again. Too high. ')
-        else:
-            print('Yay,com')
+    print(f'Yay, congrats. You have guessed the number {random_number} correctly')
 
-guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f' Is {guess} too high (H), too low (L), or correct (C)')
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
+    
+computer_guess(10)
