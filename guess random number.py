dbd88df9794d0b438c2033dd0c4e2f05936f5fe3
srivasihnavix <vinnybhamidipati@gmail.com>
2023-12-22 00:11:43 +0530

    debugging done for guess random number

--- guess random number.py
+++ guess random number.py
@@ -3,11 +3,8 @@ import random
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
-
     while guess != random_number:
-        guess = input(f'Guess a number between 1 and {x}:')
-        print(guess)
-
+        guess = int(input(f'Guess a number between 1 and {x}:'))
         if guess < random_number:
             print('Sorry, guess again. Too low. ')
         elif guess > random_number:
@@ -24,7 +21,7 @@ def computer_guess(x):
             guess = random.randint(low, high)
         else:
             guess = low
-        feedback = input(f' Is {guess} too high (H), too low (L), or correct (C)')
+        feedback = input(f' Is {guess} too high (H), too low (L), or correct (C)?')
         if feedback == 'h':
             high = guess -1
         elif feedback == 'l':
@@ -32,5 +29,4 @@ def computer_guess(x):
 
     print(f'Yay! The computer guessed your number, {guess}, correctly!')
 
-    
-computer_guess(10)
+computer_guess(1000)
